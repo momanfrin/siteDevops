0e292b6337750479e52d4b20be844360f546bc0d
Momanfrin <monique.manfrin@gmail.com>
2022-05-02 20:05:20 -0300

    creatHTML integracao com dynamo

--- teste/js/getProductName.js
+++ teste/js/getProductName.js
@@ -1,8 +1,49 @@
-function getProduct() {
-    let x = document.getElementById('teste-nome').value=("");
-    let valor = x
-    console.log(valor)
-    return valor
+async function getProduct() {
+    let data = {'nome': 'testeNovoItem'};
+    const response = await fetch(`https://rupyw5lqw8.execute-api.us-east-1.amazonaws.com/post`, {
+        // const response = await fetch(`https://rupyw5lqw8.execute-api.us-east-1.amazonaws.com/post?nome=vaca`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    
+      headers: {
+        'Content-Type': 'application/json',
+        
+      },
+    
+    }).then(data => {
+        data = data.json().then(data =>{
+            console.log(data)
+            let response = data.body;// Items
+            response.map((e)=>{
+                if(e.nome === 'cabrito'){
+                    let dadosItem = JSON.parse(e.valor);
+                    createHTML(e.nome, dadosItem.lanceMinimo);
+                }
+            });
+        })
+    })
 
+}
+function createHTML(nome, lanceMinimo){
+    document.getElementById(`arrematadosRow`)
+        .insertAdjacentHTML(`afterbegin`,`<div class="col">
+        <div class="card h-100 imagem-container img">
 
+          <img id="gato" src="../teste/img/gatinho.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${nome}</h5>
+            <p class="card-text">Iniciou dia <br> 15/04/2022  15:49:00<br> <br> Finaliza dia <br> ----- </p>
+            <h5>Lance inicial: ${lanceMinimo}</h5>
+            <h5>Lance final: Adote!</h5>
+            <h5>Lance atual: sua consciência!</h5>
+          </div>
+
+          <div class="card-footer">
+            <a href="" class="btn btn-primary comprar">Dar um lance</a>
+            <div class="input-group mb-3 valor-lance-produto">
+              <button class="btn btn-primary comprar" type="button" id="button-addon1">Valor</button>
+              <input type="text" class="form-control" placeholder="R$" aria-label="Example text with button addon" aria-describedby="button-addon1">
+            </div>
+          </div>
+        </div>
+      </div>`);
 }
--- teste/js/postItem.js
+++ teste/js/postItem.js
@@ -1,17 +1,18 @@
 async function postItem() {
-    let data = {"nome" : "guitar", "valor": 1000.00}
-    // Default options are marked with *
+  let nomeItem = document.getElementById('nomeDoItem').value;
+  let lanceMinimo = document.getElementById('lanceMinimo').value;
+  let objeto = {'nome': nomeItem, lanceMinimo: lanceMinimo};
+  let data = {"nome" : nomeItem, "valor": JSON.stringify(objeto)};
+  console.log(data);
+    
     const response = await fetch(`https://rupyw5lqw8.execute-api.us-east-1.amazonaws.com/post`, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    //   mode: 'cors', // no-cors, *cors, same-origin
-    //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //   credentials: 'same-origin', // include, *same-origin, omit
+    
       headers: {
         'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
+      
       },
-    //   redirect: 'follow', // manual, *follow, error
-    //   referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    
       body: JSON.stringify(data) // body data type must match "Content-Type" header
     }).then(data => {
         data = data.json().then(data =>{
